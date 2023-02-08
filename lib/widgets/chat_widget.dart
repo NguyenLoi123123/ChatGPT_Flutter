b8ac46ea7d7ca8339d6363c477cce95390635295
Hong Loi <hongloi123123@gmail.com>
2023-02-08 15:23:22 +0700

    execute chat

--- lib/widgets/chat_widget.dart
+++ lib/widgets/chat_widget.dart
@@ -1,3 +1,4 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:chatgpt/constants/constants.dart';
 import 'package:chatgpt/services/assets_manager.dart';
 import 'package:flutter/material.dart';
@@ -31,9 +32,26 @@ class ChatWidget extends StatelessWidget {
                   width: 8,
                 ),
                 Expanded(
-                  child: TextWidget(
-                    label: msg,
-                  ),
+                  child: chatIndex == 0
+                      ? TextWidget(
+                          label: msg,
+                        )
+                      : DefaultTextStyle(
+                          style: const TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w700,
+                              fontSize: 16),
+                          child: AnimatedTextKit(
+                              isRepeatingAnimation: false,
+                              repeatForever: false,
+                              displayFullTextOnTap: true,
+                              totalRepeatCount: 1,
+                              animatedTexts: [
+                                TyperAnimatedText(
+                                  msg.trim(),
+                                ),
+                              ]),
+                        ),
                 ),
                 chatIndex == 0
                     ? const SizedBox.shrink()
@@ -48,8 +66,10 @@ class ChatWidget extends StatelessWidget {
                           SizedBox(
                             width: 5,
                           ),
-                          Icon(Icons.thumb_down_alt_outlined,
-                              color: Colors.white)
+                          Icon(
+                            Icons.thumb_down_alt_outlined,
+                            color: Colors.white,
+                          )
                         ],
                       ),
               ],
--- lib/widgets/drop_down.dart
+++ lib/widgets/drop_down.dart
@@ -1,6 +1,13 @@
+import 'package:chatgpt/providers/models_provider.dart';
+import 'package:chatgpt/widgets/text_widget.dart';
+import 'package:chatgpt/providers/models_provider.dart';
+import 'package:chatgpt/services/api_service.dart';
+import 'package:chatgpt/widgets/text_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../constants/constants.dart';
+import '../models/models_model.dart';
 
 class ModelsDrowDownWidget extends StatefulWidget {
   const ModelsDrowDownWidget({super.key});
@@ -10,18 +17,44 @@ class ModelsDrowDownWidget extends StatefulWidget {
 }
 
 class _ModelsDrowDownWidgetState extends State<ModelsDrowDownWidget> {
-  String currentModel = "Model1";
+  String? currentModel;
   @override
   Widget build(BuildContext context) {
-    return DropdownButton(dropdownColor: scaffoldBackgroundColor,
-    iconEnabledColor: Colors.white,
-      items: getModelsItem,
-      value: currentModel,
-      onChanged: (value) {
-        setState(() {
-          currentModel = value.toString();
+    final modelsProvider = Provider.of<ModelsProvider>(context, listen: false);
+    currentModel = modelsProvider.getCurrentModel;
+    return FutureBuilder<List<ModelsModel>>(
+        future: modelsProvider.getAllModels(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Center(
+              child: TextWidget(label: snapshot.error.toString()),
+            );
+          }
+          return snapshot.data == null || snapshot.data!.isEmpty
+              ? const SizedBox.shrink()
+              : FittedBox(
+                  child: DropdownButton(
+                    dropdownColor: scaffoldBackgroundColor,
+                    iconEnabledColor: Colors.white,
+                    items: List<DropdownMenuItem<String>>.generate(
+                        snapshot.data!.length,
+                        (index) => DropdownMenuItem(
+                            value: snapshot.data![index].id,
+                            child: TextWidget(
+                              label: snapshot.data![index].id,
+                              fontSize: 15,
+                            ))),
+                    value: currentModel,
+                    onChanged: (value) {
+                      setState(() {
+                        currentModel = value.toString();
+                      });
+                      modelsProvider.setCurrentModel(
+                        value.toString(),
+                      );
+                    },
+                  ),
+                );
         });
-      },
-    );
   }
 }
